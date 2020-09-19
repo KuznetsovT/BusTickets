eaa946ebeaddd8f5693a65a71ddbfd8b5ccb6465
Терентий <ya.terenti.ak@yandex.ru>
2020-09-19 20:44:36 +0700

    quick evaluator

--- BusTickets/Main.cpp
+++ BusTickets/Main.cpp
@@ -2,63 +2,29 @@
 
 #include <iostream>
 #include <fstream>
+#include <memory>
 
-#define length 6
-#define _goal 100
 
 int main() {
-	unsigned arr[length] = { 0, 0, 0, 0, 0, 0 };
-	std::cout << "INPUT. PRINT 6 NATURAL NUMBERS: ";
-	for (auto& i : arr) { std::cin >> i; }
-	TicketsSolver ts(length, _goal, arr);
 
-	//хочу чтобы выводились только тривиальные решения без деления.
-	ts.permutator.OPERATORS_COUNT = TicketsSolver::Permutator::NO_DIVISION;
+	std::cout << "INPUT <- PRINT LENGTH AND GOAL YOU NEED : ";
+	unsigned length = 0; int _goal = 0;
+	std::cin >> length >> _goal;
+	std::cout << "\nPRINT " << length << " RATIONAL NUMBERS TO EVALUATE : ";
+	std::unique_ptr<unsigned[]> arr = std::make_unique<unsigned[]>(length);
+	for (unsigned i = 0; i < length; i++) { std::cin >> arr[i]; }
+	try {
+		TicketsSolver ts(length, _goal, arr.get());
 
-	//потом я хочу вывести решения в обратной польской нотации
+		std::cout << "\nOUTPUT ->\n";
+		//выводим все решения в поток
+		unsigned count = ts.all_solutions(std::cout);
+		std::cout << "\nCOUNT OF SOLUTIONS IS : " << count << std::endl;
 
-	ts.all_solutions(std::cout, TicketsSolver::REVERSED_NOTATION);
-
-	/*
-	* но готовый метод - слишком легко, тем более что TicketsSolver  состоит из трёх классов:
-	*	permutator, evaluator и str_converter, 
-	*	у которых есть публичные методы позволяющие писать свои переборы, отличные от уже реализованных.
-	* Сейчас я попробую это сделать.
-	*/
-
-	/*
-			for (ts.permutator.reinit_signs();
-				ts.permutator.are_signs_valid(); 
-				ts.permutator.next_sign_configuration()) 
-			{
-				ts.permutator.reinit_pos();
-				while (ts.permutator.is_doubled()) ts.permutator.next_operators_permutation();
-				while (ts.permutator.are_poses_valid()) {
-					if (ts.goal == ts.evaluator.evaluate()) {
-						std::cout << ts.goal << " = " << ts.str_converter.convert(TicketsSolver::REVERSED_NOTATION) << std::endl;
-					}
-					do ts.permutator.next_operators_permutation(); while (ts.permutator.is_doubled());
-				}
-			}
-	*/
-
-
-	/*
-	Так же, если лень писать,
-	можно воспользоваться методами которые ищут решение и сохраняют в себе конфигурацию операторов.
-	Я мог сделать нечто такое:
-	*/
-
-	/*
-			if (ts.find_first_solution()) do {
-
-				//здесь могла быть ваша реклама
-					std::cout << ts.goal << " = " << ts.str_converter.convert(TicketsSolver::REVERSED_NOTATION) << std::endl;
-
-			} while (ts.next_solution());
-
-	*/
-	
+	} catch (std::exception e) {
+		std::cout << "\nEXCEPTION { " << e.what() << " }\n";
+		return -1;
+	}
 	
 	return 0;
 }
